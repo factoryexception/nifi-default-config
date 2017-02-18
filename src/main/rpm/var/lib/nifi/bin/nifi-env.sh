f450644afc74b2ed4b220e1e72bd4f8657a9ff97
factoryexception <factoryexception@gmail.com>
2017-02-17 19:46:46 -0500

    Removed startup scripts in favor of copying distribution

--- src/main/rpm/var/lib/nifi/bin/nifi-env.sh
+++
@@ -1,6 +0,0 @@
-export JAVA_HOME=/etc/alternatives/jre
-
-export NIFI_HOME=$(cd "${SCRIPT_DIR}" && cd .. && pwd)
-
-export NIFI_PID_DIR="${NIFI_HOME}/run"
-export NIFI_LOG_DIR="${NIFI_HOME}/logs"
--- src/main/script/postinstall.sh
+++ src/main/script/postinstall.sh
@@ -1,2 +1,18 @@
+NIFI_HOME=/var/lib/nifi
+NIFI_HOME_BIN=$NIFI_HOME/bin
+NIFI_DIST=/usr/share/nifi-distribution
+NIFI_DIST_BIN=$NIFI_DIST/bin
+NIFI_SH=nifi.sh
+NIFI_ENV_SH=nifi-env.sh
+
+NIFI_USER=nifi
+NIFI_GROUP=nifi
+
+cp $NIFI_DIST_BIN/$NIFI_SH $NIFI_HOME_BIN/$NIFI_SH
+cp $NIFI_DIST_BIN/$NIFI_ENV_SH $NIFI_HOME_BIN/$NIFI_ENV_SH
+
+chown $NIFI_USER:$NIFI_GROUP $NIFI_HOME_BIN/$NIFI_SH
+chown $NIFI_USER:$NIFI_GROUP $NIFI_HOME_BIN/$NIFI_ENV_SH
+
 systemctl daemon-reload
 systemctl enable nifi.service
